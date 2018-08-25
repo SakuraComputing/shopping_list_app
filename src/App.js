4559038e829f4d663bd89ec641cb35a39820d017
Ewan Bauld <ewan.bauld@gmail.com>
2018-08-25 08:23:19 +0100

    Style Colors and logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './styles/styles.css';
 import Menu from './components/Menu';
-import Shopping from './components/Shopping'
+import Shopping from './components/Shopping';
+import AOLogoGreen from './styles/img/ao-header-logo.jpg';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
-          <h1 className="App-title">Shopping List</h1>
+          <img className="ao-header-logo" src={AOLogoGreen} alt="ao header logo"/>
+          <div className="App-basket">Basket</div>
         </header>
         <Menu />
         <Shopping />
